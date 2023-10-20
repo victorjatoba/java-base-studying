208bbae28e9d13c71f42cca3cf7dea9edadb4e34
vitor-m-jatoba-alb <vitor-m-jatoba@alticelabs.com>
2023-10-20 16:32:54 +0100

    overloading vs overriding

---
+++ MethodOverloadingEx.java
@@ -0,0 +1,14 @@
+public class MethodOverloadingEx {
+    static int add (int a, int b) {
+        return a + b;
+    }
+
+    static int add (int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main (String args[]) {
+        System.out.println(add(4, 6));
+        System.out.println(add(4, 6, 7));
+    }
+}
